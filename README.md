34c1df3749b6547d1a57312ace35bb34e77caba2
Giovanni Alfredo Garciliano Díaz <rapunzel@disroot.org>
2023-07-11 05:24:33 -0600

    Añadiendo trabajo, no listo

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-# CodeMirror 6 language package template
+# Z80 language package for CodeMirror 6
 
-This is an example repository containing a minimal [CodeMirror](https://codemirror.net/6/) language support package. The idea is to clone it, rename it, and edit it to create support for a new language.
-
-Things you'll need to do (see the [language support example](https://codemirror.net/6/examples/lang-package/) for a more detailed tutorial):
-
- * `git grep EXAMPLE` and replace all instances with your language name.
-
- * Rewrite the grammar in `src/syntax.grammar` to cover your language. See the [Lezer system guide](https://lezer.codemirror.net/docs/guide/#writing-a-grammar) for information on this file format.
-
- * Adjust the metadata in `src/index.ts` to work with your new grammar.
-
- * Adjust the grammar tests in `test/cases.txt`.
-
- * Build (`npm run prepare`) and test (`npm test`).
-
- * Rewrite this readme file.
-
- * Optionally add a license.
-
- * Publish. Put your package on npm under a name like `codemirror-lang-EXAMPLE`.
+This package adds support to CM6 for the Z80 assembly package, with the syntax specified and processed by [this program](https://www.github.com/twilight1794/z80), very close to the implemented by Cross-32 Meta Assemblier.
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {LRLanguage, LanguageSupport} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
+import {completeFromList} from "@codemirror/autocomplete"
+
+export const Z80Language = LRLanguage.define({
+    parser: parser.configure({
+        props: [
+            styleTags({
+                Eti: t.name,
+                Cad: t.string,
+                Mnemo: t.keyword,
+                Num: t.number,
+                Com: t.lineComment,
+                Paren: t.paren,	    
+            })
+       ]
+    }),
+    languageData: {
+        commentTokens: {line: ";"}
+    }
+});
+
+export const Z80Completion = Z80Language.data.of({
+    autocomplete: completeFromList([
+	{label: "adc", type: "keyword"},
+	{label: "add", type: "keyword"},
+	{label: "and", type: "keyword"},
+	{label: "cpu", type: "function"},
+	{label: "db", type: "function"},
+	{label: "end", type: "function"},
+	{label: "hof", type: "function"},
+	{label: "org", type: "function"}
+    ])
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function Z80() {
+    return new LanguageSupport(Z80Language, [Z80Completion])
 }
